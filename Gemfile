272d6f3a7a93178a77babf3570ce2341c2f65d55
ucheol2 <cokr3430@gmail.com>
2016-06-20 23:23:03 +0900

    will paginate setting

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'faker'
+gem 'will_paginate'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.search
+    @users = User.search.paginate(page: params[:page])
 
     # This is action multi format render
     # We'll cover it soon
