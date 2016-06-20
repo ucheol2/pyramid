dcc055c5a1845da01d361736e7548b441a69299f
ucheol2 <cokr3430@gmail.com>
2016-06-20 22:55:24 +0900

    add multi format render to index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
 
   def index
     @users = User.search
+
+    # This is action multi format render
+    # We'll cover it soon
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
 end
